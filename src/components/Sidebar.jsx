f8a62c341601441e7a6dcc205c1c6b9b7c7da200
becauseoffyou <irhandy96@gmail.com>
2026-09-08 17:09:57 +0700

    fix logo path

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import kanchaLogo from "../../../kancha-rental-admin/src/assets/logo_kancha.png";
+import kanchaLogo from "../assets/logo_kancha.png";
 import {
     FiHome,
     FiCalendar,
