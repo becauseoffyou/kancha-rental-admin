9b572e5267f905a9fd2e867ed95a7503971efadb
becauseoffyou <irhandy96@gmail.com>
2026-09-09 12:20:35 +0700

    add url hardcore

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,4 +1,5 @@
-const API_URL = "http://kancha-rental-api-production.up.railway.app/api";
+const API_URL =
+    import.meta.env.VITE_API_URL || "http://localhost:9090/api";
 
 const getWaitingPayments = async () => {
     const response = await fetch(
